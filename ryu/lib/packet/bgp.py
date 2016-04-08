7b77f25203130eac00bf01bbec18e98b8bd4861a
Jason Kölker <jason@koelker.net>
2016-04-08 20:55:52 +0000

    packet/bgp: Allow RouteFamily to be dictionary keys

--- ryu/lib/packet/bgp.py
+++ ryu/lib/packet/bgp.py
@@ -579,6 +579,12 @@ class RouteFamily(StringifyMixin):
     def __eq__(self, other):
         return (self.afi, self.safi) == (other.afi, other.safi)
 
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash((self.afi, self.safi))
+
 # Route Family Singleton
 RF_IPv4_UC = RouteFamily(addr_family.IP, subaddr_family.UNICAST)
 RF_IPv6_UC = RouteFamily(addr_family.IP6, subaddr_family.UNICAST)
